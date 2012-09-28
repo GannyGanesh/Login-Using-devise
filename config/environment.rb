0c3bb25f3ceefe1b710c0c0f8537740779f6c726
User <user@client>
2012-09-28 14:00:12 +0530

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,15 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Logi::Application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => 'gmail.com',
+  :user_name            => 'ganny.gan@gmail.com',
+  :password             => 'sacredbishop',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  
+  }
+ActionMailer::Base.delivery_method = :smtp
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logi::Application.config.secret_token = '3c5afb3c57436624e0d622ac15b72028729b3f58e57debd8e3b4c883c5350af89ea02f0fa504b5319cba89a92ba8d57bbd46918cb6c87011cbb6910c602b107e'
