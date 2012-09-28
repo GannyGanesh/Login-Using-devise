2ed765bbc704dc62b5488ca1e9df2c3219cd9bed
GannyGanesh <ganeshsamyk@gmail.com>
2012-09-28 14:11:48 +0530

    Update config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => 'gmail.com',
   :user_name            => 'ganny.gan@gmail.com',
-  :password             => 'sacredbishop',
+  :password             => '**************',
   :authentication       => 'plain',
   :enable_starttls_auto => true  
   }
